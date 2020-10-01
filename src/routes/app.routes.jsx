48cd36e34115fcadd956441a60400c4d6fbf5d17
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-01 15:55:49 -0300

    Add routes/app.routes  Dashboard - Tarefas

---
+++ src/routes/app.routes.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Dashboard from '../pages/Dashboard';
+import Tarefas from '../pages/Tarefas';
+
+const App = createStackNavigator();
+
+const AppRoutes = () => (
+  <App.Navigator screenOptions={{
+    headerShown: false,
+  }}
+  >
+    <App.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen name="Tarefas" component={Tarefas} />
+  </App.Navigator>
+);
+
+export default AppRoutes;
