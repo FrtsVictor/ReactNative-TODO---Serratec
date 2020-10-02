3da47bfc48c68403443fff543eb0a47b6b803db1
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-02 15:29:24 -0300

    Added BottomTabNavigator at routes/app.routes

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import Dashboard from '../pages/Dashboard';
 import Tarefas from '../pages/Tarefas';
 
-const App = createStackNavigator();
+const App = createBottomTabNavigator();
 
 const AppRoutes = () => (
-  <App.Navigator screenOptions={{
-    headerShown: false,
-  }}
+  <App.Navigator
+    initialRouteName="Tarefas"
   >
-    <App.Screen name="Dashboard" component={Dashboard} />
-    <App.Screen name="Tarefas" component={Tarefas} />
+    <App.Screen
+      name="Tarefas"
+      component={Tarefas}
+      options={{
+        tabBarIcon: () => (
+          <Ionicons name="ios-list-box" size={32} color="black" />
+        ),
+      }}
+    />
+
+    <App.Screen
+      name="Dashboard"
+      component={Dashboard}
+      options={{
+        tabBarIcon: () => (
+          <Ionicons name="ios-podium" size={32} color="black" />
+        ),
+      }}
+    />
   </App.Navigator>
 );
 
